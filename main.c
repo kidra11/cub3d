1b37e69b43ed34a8b7562c099968301e55b5cf37
Nathalie Sion <nsion@c2r4p4.42perpignan.fr>
2023-12-21 18:25:05 +0100

    is_cub_file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nsion <nsion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 16:14:23 by nsion             #+#    #+#             */
-/*   Updated: 2023/12/21 18:19:01 by nsion            ###   ########.fr       */
+/*   Updated: 2023/12/21 18:24:51 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,18 @@ void	init_maps(char *file)
 	char *line;
 	
 	if (is_cub_file(file) == 0)
-		{	
-			printf("%s\n", file);
-			fd = open(file, O_RDONLY);
-			if(!fd)
-				exit(printf("Error : file does not exist\n"));
-			line = get_next_line(fd);
-			if(!line)
-				exit(printf("Error : can't read this file\n"));
-			printf("%s\n", line);
-		}
-		else
-			exit(printf("Error : is not a .cub file\n"));
+	{	
+		printf("%s\n", file);
+		fd = open(file, O_RDONLY);
+		if(!fd)
+			exit(printf("Error : file does not exist\n"));
+		line = get_next_line(fd);
+		if(!line)
+			exit(printf("Error : can't read this file\n"));
+		printf("trier la maps is ok?\n");
+	}
+	else
+		exit(printf("Error : is not a .cub file\n"));
 }
 
 int	main(int ac, char **av)
