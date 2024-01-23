6f95296a85051288ac422c33247c6ced575e89e4
kidra11 <nathalie1116kubo@gmail.com>
2024-01-23 11:34:47 +0100

    version ludo

---
+++ cub3d.h
@@ -0,0 +1,171 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lthong <lthong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/21 15:41:31 by bbach             #+#    #+#             */
+/*   Updated: 2024/01/21 22:15:33 by lthong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "./includes/mlx/mlx.h"
+# include "./includes/libft/libft.h"
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# define SCREEN_WIDTH 1024
+# define SCREEN_HEIGHT 510
+
+# define PI 3.14159265359
+# define DR 0.0174533
+
+typedef struct s_img
+{
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_img;
+
+typedef struct s_ray
+{
+	double		ra;
+	double		rx;
+	double		ry;
+}				t_ray;
+
+typedef struct s_player
+{
+	double		pos_x;
+	double		pos_y;
+	double		pdx;
+	double		pdy;
+	double		pa;
+}				t_player;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*mlx_win;
+	int			red_f;
+	int			green_f;
+	int			blue_f;
+	int			red_c;
+	int			green_c;
+	int			blue_c;
+	char		*no;
+	char		*so;
+	char		*we;
+	char		*ea;
+	int			map_width;
+	int			map_height;
+	int			player_pos_x;
+	int			player_pos_y;
+}				t_data;
+
+typedef struct s_cub
+{
+	int			lines_count;
+	char		**all;
+	char		**map;
+	char		**tex;
+	char		**colors;
+	t_player	player;
+	t_data		data;
+	t_img		img;
+	t_ray		ray;
+}				t_cub;
+
+//clean_exit.c
+void	clean_all_exit(char *message, t_cub *cub);
+void	clean_exit(char *message, t_cub *cub);
+void	end_exit(t_cub *cub);
+void	free_tab(char **tab);
+
+//check_colors_1.c
+void	two_commas_only(t_cub *cub);
+void	check_first_letter(t_cub *cub);
+void	three_spaces_only(t_cub *cub);
+void	check_format(t_cub *cub);
+void	check_colors(t_cub *cub);
+
+//check_colors_2.c
+void	get_f_color(t_cub *cub, int i);
+void	get_c_color(t_cub *cub, int i);
+void	check_if_between_0_255(t_cub *cub);
+
+int		rgb(int r, int g, int b);
+
+
+//check_textures.c
+void	check_1st_letter(t_cub *cub);
+void	check_texture_path_1(t_cub *cub);
+void	get_path_texture(t_cub *cub);
+void	check_path_correct(t_cub *cub);
+void	check_texture(t_cub *cub);
+
+//check_map.c
+void	flood_the_wall(t_cub *cub);
+void	check_map(t_cub *cub);
+
+//stock_elem.c
+void	elements_in_map(t_cub *cub);
+int		count_elem(t_cub *cub, int choise);
+void	text_map_malloc(t_cub *cub);
+void	stock_elem(t_cub *cub);
+
+//check_syntax.
+void	map_end(t_cub *cub);
+void	duble(t_cub *cub);
+int		find_map(char **str);
+void	check_syntax(t_cub *cub);
+
+//stock.c
+char	*ft_copy(const char *str, int start, int end);
+int		count_lines(char *file);
+char	**stock(char *file);
+
+//main.c
+void	init_all_val(t_cub *cub);
+void	init_file(t_cub *cub, char *file);
+void	is_cub_file(char *file);
+
+//testeur.c
+void	printf_tab(char **all, char *name);
+
+//init.c
+void	init_all_val(t_cub *cub);
+void	init_file(t_cub *cub, char *file);
+void	init_player(t_cub *cub);
+void	init_ray(t_cub *cub);
+
+//handle_key.c
+int		key_move(int keycode, t_cub *cub);
+int		key_move_release(int keycode, t_cub *cub);
+
+//draw.c
+void	my_mlx_pixel_put(t_img *data, int x, int y, int color);
+void	draw_map(t_cub *cub);
+void	draw_player(t_cub *cub);
+void	draw_point(t_cub *cub, int x, int y, int size, int color);
+void	draw_line(t_cub *cub, int x1, int y1, int x2, int y2, int color);
+
+//raycast.c
+void	draw_ray(t_cub *cub, int psize);
+void	draw_thick_line(t_cub *cub, int x1, int y1, int x2, int y2, int thickness, int color);
+
+int		end(t_cub *cub);
+
+int		get_mapx_size(t_cub *cub);
+int		get_mapy_size(t_cub *cub);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsion <nsion@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/24 17:03:38 by nsion             #+#    #+#             */
+/*   Updated: 2024/01/22 18:36:19 by nsion            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+void	is_cub_file(char *file)
+{
+	int	i;
+
+	i = 0;
+	while (file[i])
+		i++;
+	if (file[i - 1] != 'b' || file[i - 2] != 'u' || file[i - 3] != 'c'\
+		|| file[i - 4] != '.')
+	{
+		ft_printf("Error\nWrong file extension\n");
+		exit(0);
+	}
+}
+
+int	get_mapx_size(t_cub *cub)
+{
+	int	i;
+	int	j;
+	int	count;
+	int	x_max;
+
+	i = 0;
+	x_max = 0;
+	while (cub->map[i])
+	{
+		j = 0;
+		count = 0;
+		while (cub->map[i][j])
+		{
+			if (cub->map[i][j] == '1' || cub->map[i][j] == '0'
+				|| cub->map[i][j] == ' ')
+				count++;
+			j++;
+		}
+		if (count > x_max)
+			x_max = count;
+		i++;
+	}
+	return (x_max);
+}
+
+int	get_mapy_size(t_cub *cub)
+{
+	int	i;
+
+	i = 0;
+	while (cub->map[i])
+		i++;
+	return (i);
+}
+
+int	end(t_cub *cub)
+{
+	mlx_destroy_image(cub->data.mlx, cub->img.img);
+	mlx_loop_end(cub->data.mlx);
+	mlx_destroy_window(cub->data.mlx, cub->data.mlx_win);
+	mlx_destroy_display(cub->data.mlx);
+	free(cub->data.mlx);
+	end_exit(cub);
+	return (0);
+}
+int	print_key(int keycode, t_cub *cub)
+{
+	(void)cub;
+	printf("keycode: %d\n", keycode);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	t_cub	cub;
+
+	if (ac != 2)
+		return (ft_printf("Error : Wrong argument.\n"));
+	is_cub_file(av[1]);
+	init_all_val(&cub);
+	init_file(&cub, av[1]);
+	ft_printf("Parsing done\n");
+
+	cub.data.map_width = get_mapx_size(&cub);
+	cub.data.map_height = get_mapy_size(&cub);
+	int i = 0;
+	while (cub.map[i])
+	{
+		ft_printf("%s\n", cub.map[i]);
+		i++;
+	}
+
+	cub.data.mlx = mlx_init();
+	cub.data.mlx_win = mlx_new_window(cub.data.mlx, SCREEN_WIDTH, SCREEN_HEIGHT, "cub3D");
+	init_player(&cub);
+	cub.img.img = mlx_new_image(cub.data.mlx, SCREEN_WIDTH, SCREEN_HEIGHT);
+	cub.img.addr = mlx_get_data_addr(cub.img.img, &cub.img.bits_per_pixel, &cub.img.line_length, &cub.img.endian);
+	draw_map(&cub);
+	draw_player(&cub);
+	printf("Player position: (%f, %f)\n", cub.player.pos_x, cub.player.pos_y);
+	printf("Player direction: %f radians\n", cub.player.pa);
+	printf("Ray direction: %f radians\n", cub.ray.ra);
+	//mlx_key_hook(cub.data.mlx_win, print_key, &cub);
+	mlx_hook(cub.data.mlx_win, 17, 0, end, &cub);
+	mlx_hook(cub.data.mlx_win, 2, 1L<<0, key_move, &cub);
+	mlx_hook(cub.data.mlx_win, 3, 1L<<1, key_move_release, &cub);
+	mlx_loop(cub.data.mlx);
+
+	end_exit(&cub);
+	return (0);
+}
+
+//main = on va tout check et initialiser et nettoyer pour bien fermer
+//is_cub_file = on check si c'est un fichier lisible
+//init_file = on stock tout le ficher et check si il est correct et 
+//le separer en 3 partie
+//faire un init des variable init_variable
