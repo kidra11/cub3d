7a6d777ce4f69ec64a934d6cbf39bf94f3cfbc5d
Ludovic Thong <ludo@MacBook-Pro-de-Ludovic.local>
2024-01-30 17:16:54 +0100

    added project texture

---
+++ Makefile
@@ -0,0 +1,86 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lthong <lthong@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/12/21 15:28:04 by nsion             #+#    #+#              #
+#    Updated: 2024/01/25 01:33:18 by lthong           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+SRC = main.c\
+	stock.c\
+	check_syntax.c\
+	stock_elem.c\
+	check_map.c\
+	check_texture.c\
+	check_colors_1.c\
+	check_colors_2.c\
+	clean_exit.c\
+	testeur.c\
+	init.c\
+	draw.c\
+	raycast1.c\
+	raycast2.c\
+	render.c\
+	handle_key.c\
+
+OBJS = $(addprefix $(OBJ_DIR), $(SRC:.c=.o))
+
+SRC_DIR = src/
+OBJ_DIR = obj/
+
+RM = rm -rf
+
+NAME = cub3D
+
+LMLX = ./mlx/libmlx.a
+
+LIB = ./libft/libft.a
+
+CC = gcc
+
+CFLAGS = -Wall -Werror -Wextra -g
+
+MLXFLAGS = -I/usr/includes -Imlx -lmlx -L/usr/lib/
+MLXMAC = -I /usr/X11/include -g -L /usr/X11/lib -l mlx -framework OpenGL -framework AppKit
+
+all : $(LIB) $(LMLX) $(NAME) 
+
+$(LMLX):
+	@make -s -C ./mlx/ all
+
+$(LIB) :
+	@make -s -C ./libft/ all
+
+$(OBJ_DIR)%.o : $(SRC_DIR)%.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME) : $(OBJS) $(LIB) $(LMLX)
+	@$(CC) $(CFLAGS) $(LIB) $(MLXFLAGS) $(OBJS) -o $(NAME)
+	@echo "\033[32mDone !\033[0m"
+
+clean :
+	@$(RM) $(OBJ_DIR)
+	@make -s -C ./libft/ 
+	@make -s -C ./mlx/ clean
+
+fclean : clean
+	@$(RM) $(NAME)
+	@make -s -C ./libft/ 
+	@make -s -C ./mlx/
+
+re : fclean all
+
+mac : $(OBJS) $(LIB) $(LMLX)
+	@$(CC) $(CFLAGS) $(LIB) $(MLXMAC) $(OBJS) -o $(NAME)
+	@echo "\033[32mDone !\033[0m"
+
+debug : $(LIB) $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) -L./includes/libft -lft -o $(NAME) -g
+	@echo "033[32mDone !\033[0m"
+
+.PHONY : all clean fclean re mac
---
+++ src/stock.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stock.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lthong <lthong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 15:29:58 by nsion             #+#    #+#             */
+/*   Updated: 2024/01/23 20:13:30 by lthong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+char	*ft_copy(const char *str, int start, int end)
+{
+	int		i;
+	char	*cp;
+
+	i = 0;
+	cp = (char *) malloc(((end - start) + 1) * sizeof(char));
+	if (!cp)
+		return (NULL);
+	while (start < end)
+	{
+		if (str[start] == '\n')
+			start++;
+		cp[i++] = str[start++];
+	}
+	cp[i] = 0;
+	return (cp);
+}
+
+int	count_lines(char *file)
+{
+	int		fd;
+	int		lines_count;
+	char	*line;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		exit(ft_printf("Error opening file"));
+	lines_count = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		lines_count++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (lines_count);
+}
+
+char	**stock(char *file)
+{
+	int		fd;
+	char	*line;
+	char	**all;
+	int		i;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		exit(printf("Error : file does not exist\n"));
+	all = malloc((count_lines(file) + 1) * sizeof(char *));
+	if (!all)
+	{
+		close(fd);
+		return (NULL);
+	}
+	i = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		all[i++] = ft_copy(line, 0, ft_strlen(line));
+		free(line);
+		line = get_next_line(fd);
+	}
+	all[i] = NULL;
+	close(fd);
+	return (all);
+}
+
+//on va stocker tout le ficher dans char **all.
+//stock = on va lire et copier le fichier (utilisation de gnl)
+//count_lines = on va compter combier all aura de ligne (utilisation de gnl)
+//ft_copy = on malloc et copie chaque caratere de la ligne. 
